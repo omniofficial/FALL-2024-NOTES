75f5b088257344af6333b70febeb51e4b8738b48
omniofficial <omniofficial04@gmail.com>
2024-09-20 18:13:24 -0700

    New program file

--- CPSC351/DEVELOPMENT/assignment1.cpp
+++ CPSC351/DEVELOPMENT/assignment1.cpp
@@ -1,8 +1,9 @@
 // -------------------------------
 // Assignment: Number #1 - Parallel Text Processing
 // Due date: 9/24/24
-// Purpose: Write a C/C++ program that uses multiple threads to count the number of vowels in separate strings. Each thread will process one string and compute the count of vowels in it. 
+// Purpose: Write a C/C++ program that uses multiple threads to count the number of vowels in separate strings. Each thread will process one string and compute the count of vowels in it.
 // -------------------------------
 
-
-
+// Create an array of string. EX: ["Hello", "World]
+// Thread1, Thread2
+// Separate char values, EX: H, e, l, etc...
